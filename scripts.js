a470c97aa7a05a5ef3e25801f79217488bef5d76
BarathAathiraj <barathaathiraj33@gmail.com>
2024-06-13 00:13:24 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.querySelector(".toggle");
+    const navigation = document.querySelector(".navigation");
+  
+    toggle.addEventListener("click", () => {
+      toggle.classList.toggle("active");
+      navigation.classList.toggle("active");
+    });
+  });
+  
